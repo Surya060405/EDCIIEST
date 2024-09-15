efcd4af1b539ef8ce813c9c18df7c92b739fc198
Harsh Ranjan <shriharshranjangupta@gmail.com>
2024-09-15 19:24:34 +0530

    photo

--- src/app/home/Ourteam.tsx
+++ src/app/home/Ourteam.tsx
@@ -1,33 +1,47 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Ourteamcss.module.css";
 import Biocard from "../components/Biocard";
 import data from "../schema/team";
 
 const Ourteam = () => {
+  const [showAll, setShowAll] = useState(false); // State to toggle view
+
+  // Toggle between showing all or limited team members
+  const toggleShowTeam = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div id="ourteam" className={styles.ourteam}>
       <header className={styles.header}>Our Team</header>
+
       <div className={styles.cardholder}>
-        {data.map((item, index) => {
-          return (
-            <div className={styles.card} key={index}>
-              <Biocard
-                id={item.id}
-                name={item.name}
-                role={item.role}
-                description={item.description}
-                facebooklink={item.facebooklink}
-                instalink={item.instalink}
-                twitterlink={item.twitterlink}
-                email={item.email}
-                linkedin = {item.linkedin}
-                imgsource={item.imgsource}
-              />
-            </div>
-          );
-        })}
+        {/* Conditionally show limited or all team members */}
+        {data.slice(0, showAll ? data.length : 4).map((item, index) => (
+          <div className={styles.card} key={index}>
+            <Biocard
+              id={item.id}
+              name={item.name}
+              role={item.role}
+              description={item.description}
+              facebooklink={item.facebooklink}
+              instalink={item.instalink}
+              twitterlink={item.twitterlink}
+              email={item.email}
+              linkedin={item.linkedin}
+              imgsource={item.imgsource}
+            />
+          </div>
+        ))}
       </div>
-      <button className={styles.button}>Explore</button>
+
+      {/* Button to toggle between showing more or less team members */}
+      <div className={styles.viewmorecontainer}>
+        <button className={styles.viewMoreButton} onClick={toggleShowTeam}>
+          {showAll ? "View Less" : "View More"}
+        </button>
+      </div>
     </div>
   );
 };
